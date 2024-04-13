02abfd1f48bd4692f05879785a5194280871f584
Zulkh87-AF-K <156850017+zulkh87@users.noreply.github.com>
2024-04-13 23:47:06 +0300

    light/dark mode javascript functionality added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+/* Light and Dark Mode Toggle */
+
+const $themeBtn = document.querySelector("[data-theme-btn");
+const $HTML = document.documentElement;
+let isDark = window.matchMedia("(prefers-color-scheme:dark)").matches;
+
+if (sessionStorage.getItem("theme")) {
+    $HTML.dataset.theme = sessionStorage.getItem("theme");
+} else {
+    $HTML.dataset.theme = isDark ? "dark" : "light";
+    sessionStorage.setItem("theme", $HTML.dataset.theme);
+}
+
+const changeTheme = () => {
+    $HTML.dataset.theme = sessionStorage.getItem("theme") === "light" ? "dark" : "light";
+    sessionStorage.setItem("theme", $HTML.dataset.theme);
+}
+
+$themeBtn.addEventListener("click",changeTheme);
