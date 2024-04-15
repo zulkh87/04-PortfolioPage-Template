d7c0a282bbdc6427311fb02e08ae46075a77f27e
Zulkh87-AF-K <156850017+zulkh87@users.noreply.github.com>
2024-04-15 14:51:42 +0300

    primary tab functionality added

--- js/script.js
+++ js/script.js
@@ -19,3 +19,23 @@ const changeTheme = () => {
 }
 
 $themeBtn.addEventListener("click",changeTheme);
+
+// Primary tabs visibility functionality
+
+const $tabBtn = document.querySelectorAll("[data-tab-btn]");
+let [lastActiveTab] = document.querySelectorAll("[data-tab-content]");
+let [lastActiveTabBtn] = $tabBtn;
+
+$tabBtn.forEach(item => {
+    item.addEventListener("click", function (){
+        lastActiveTab.classList.remove("active");
+        lastActiveTabBtn.classList.remove("active");
+
+        const $tabContent = document.querySelector(`[data-tab-content="${item.dataset.tabBtn}"]`);
+        $tabContent.classList.add("active");
+        this.classList.add("active");
+
+        lastActiveTab = $tabContent;
+        lastActiveTabBtn = this;
+    })
+})
